4bd2a2a830a4b0a567f4736f2167d5a885d14d7c
Emanuel Calso <egcalso@chicharon.(none)>
2009-05-05 05:34:43 +0800

    Modified some more admin stuff;

--- upcoming/admin.py
+++ upcoming/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 class UpcomingAdmin(admin.ModelAdmin):
     fields = ['upload', 'chosen']
+    list_display = ['uploaded', 'upload', 'chosen', ]
+
 
 admin.site.register(models.UpcomingUpload, UpcomingAdmin)
 
