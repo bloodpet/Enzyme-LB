4eab6e186f1a269184a49cac798a4a1baf60f7ba
Emanuel Calso <egcalso@chicharon.(none)>
2009-05-05 03:22:30 +0800

    Added the bio model;

---
+++ biography/admin.py
@@ -0,0 +1,11 @@
+import models                     
+from django.contrib import admin  
+
+
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('year', 'award', 'location', )
+
+admin.site.register(models.Exhibition)
+admin.site.register(models.Education)
+admin.site.register(models.Award)
+
--- biography/models.py
+++ biography/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Award(models.Model):
+    award = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
+    year = models.IntegerField()
+
+    def __unicode__(self):
+        return self.award
+
+
+class Education(models.Model):
+    course = models.CharField(max_length=256, unique=True)
+    location = models.CharField(max_length=256)
+    years_taken = models.CharField(max_length=256)
+
+    def __unicode__(self):
+        return self.course
+
+
+class Exhibition(models.Model):
+    year = models.IntegerField()
+    details = models.CharField(max_length=512, unique=True)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.year, self.details)
+
